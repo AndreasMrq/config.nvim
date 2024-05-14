f1bb329b60e77c0e277e13e1b316f0b0ac6a04ea
AndreasMrq <andreas_marquardt@outlook.com>
2024-05-14 19:00:06 +0200

    update to lazy package manager

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,34 +1,45 @@
-return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use 'ThePrimeagen/vim-be-good'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use({ 'rose-pine/neovim', as = 'rose-pine' })
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
-	use {
-		'ThePrimeagen/harpoon',
-		requires = {{ 'nvim-lua/plenary.nvim'}}
-	}
-	use 'tpope/vim-fugitive'
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-)
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+{
+	{'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	dependencies = { {'nvim-lua/plenary.nvim'} }
+},
+{
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate'
+},
+{ 'rose-pine/neovim', as = 'rose-pine' },
+{
+	'ThePrimeagen/harpoon',
+	dependencies = {{ 'nvim-lua/plenary.nvim'}}
+},
+{
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v3.x',
+	dependencies = {
+		{'williamboman/mason.nvim'},
+		{'williamboman/mason-lspconfig.nvim'},
+		-- LSP Support
+		{'neovim/nvim-lspconfig'},
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},
+		{'hrsh7th/cmp-nvim-lsp'},
+		{'L3MON4D3/LuaSnip'},
+	}
+},
+
+	})
+
